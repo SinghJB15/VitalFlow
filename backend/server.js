d1b3664ab53431639471f374a141eb6f32d392e3
bhogalj15 <jazz3806@gmail.com>
2024-03-18 18:21:58 -0400

    updated settings for server.js

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(session({
     }),
     cookie: {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        secure: false,
         maxAge: 1000 * 60 * 60 * 24,
         sameSite: "None"
     }
