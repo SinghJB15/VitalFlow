53fb4e113fc93ad8524eec936a9d6afe492b1d78
bhogalj15 <jazz3806@gmail.com>
2024-03-22 21:35:27 -0400

    edited ping route

--- backend/controllers/pingController.js
+++ backend/controllers/pingController.js
@@ -3,5 +3,5 @@ const express = require("express");
 
 //Ping to allow server to awake
 exports.ping = (req, res) => {
-    res.status(200).send("Server is awake");
+    res.status(200).send("ok");
 }
