73fec8730c7d0cbb3d3dcc49074e4fd498c531d0
bhogalj15 <jazz3806@gmail.com>
2024-04-09 21:02:22 -0400

    updated package.json file for frontend deployment

--- frontend/my-app/src/components/Main.js
+++ frontend/my-app/src/components/Main.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
 import Profile from "../pages/fitbitProfile";
 import Device from "../pages/fitbitDevice";
 import Heartrate from "../pages/fitbitHeartrate";
